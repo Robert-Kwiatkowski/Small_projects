12355c525d09bb3900c7f6a7e555adbeb2b82633
Robert <seken1233123@gmail.com>
2019-04-09 21:09:03 +0200

    Added simple ShakeSort algorithm

---
+++ ShakeSort.py
@@ -0,0 +1,29 @@
+#Robert Kwiatkowski 10.11.2018
+#Algoirthms and Data Structures
+#Prosta Zmiana (ShakeSort)
+def ShakeSort(alist):
+    def swap(i, j):
+        alist[i], alist[j] = alist[j], alist[i]
+    upp = len(alist) -1
+    down = 0
+
+    no_change = False
+    while(not no_change and upp - down > 1):
+        no_change = True
+        for j in range(down, upp):
+            if alist[j+1] < alist[j]:
+                swap(j+1,j)
+                no_change = False
+        upp = upp -1
+
+        for j in range(upp, down, -1):
+            if alist[j-1] > alist[j]:
+                swap(j -1, j)
+                no_change = False
+        down = down + 1
+
+alist = [1, 3, 10, 2, 5, 4, 6, 11, 9]
+ShakeSort(alist)
+print(alist)
+
+
